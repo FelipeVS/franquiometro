c91ef30db3091b07602e8733906f5e3c5ef580b3
FelipeVS <osolevepilef@gmail.com>
2016-05-05 07:09:00 -0300

    stupid fix - The revenge

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var fs        = require('fs');
 var http      = require('http');
 var config    = require('./config/config');
 var root      = __dirname;
+var path = require('path');
 var app       = express();
 var server    = null;
 
@@ -19,6 +20,32 @@ fs.readdirSync(modelsPath).forEach(function (file) {
 });
 
 require('./config/express')(app, config);
+
+// hold all individuals routes
+var api = {};
+// require all routes
+var routesPath = root + '/routes';
+fs.readdirSync(routesPath).forEach(function (file) {
+  if (file.indexOf('.js') >= 0) {
+    var name = file.replace(/\.[^/.]+$/, "")
+    api[name] = require(routesPath + '/' + name );
+  }
+});
+
+// create all API routes
+app.use('/api/', api.index);
+app.use('/api/isps', api.isps);
+app.use('/api/users', api.users);
+
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.send('Nada aqui!')
+  next(err);
+});
 
 var server = http.createServer(app);
 server.listen(config.port);
--- config/express.js
+++ config/express.js
@@ -2,7 +2,6 @@ var compression = require('compression');
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cors = require('cors');
@@ -20,29 +19,4 @@ module.exports = function(app, config) {
   app.use(cookieParser());
 
 
-  // hold all individuals routes
-  var api = {};
-  // require all routes
-  var routesPath = __dirname + './../routes';
-  fs.readdirSync(routesPath).forEach(function (file) {
-    if (file.indexOf('.js') >= 0) {
-      var name = file.replace(/\.[^/.]+$/, "")
-      api[name] = require(routesPath + '/' + name );
-    }
-  });
-
-  // create all API routes
-  app.use('/api/', api.index);
-  app.use('/api/isps', api.isps);
-  app.use('/api/users', api.users);
-
-
-  app.use('/', express.static(path.join(__dirname, '../public')));
-
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    res.send('Nada aqui!')
-    next(err);
-  });
 };
